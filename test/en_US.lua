96467e0b4523969413ebe5f1ed8fb67975a95d5b
Hector Vido <hector_vido@yahoo.com.br>
2021-04-14 00:41:10 -0300

    Criado loaders

--- test/en_US.lua
+++ test/en_US.lua
@@ -1,88 +1,60 @@
--- pt_BR.lua
+-- en_US.lua
 
-local pt_BR = {lastnames = {}, countries = {}, states = {}, cities = {}}
+local en_US = {lastnames = {}, countries = {}, states = {}, cities = {}}
 
-function pt_BR.load_firstnames()
+function en_US.load_firstnames()
 	local firstnames = {{}, {}} -- avoid some rehashes
 	-- 1 - feminine
-	local data = assert(io.open('data/pt_BR/firstnames-feminine.csv', 'rb')):read('*all')
+	local data = assert(io.open('data/en_US/firstnames-feminine.csv', 'rb')):read('*all')
 	for name in string.gmatch(data, '[%S]+') do
 		firstnames[1][#firstnames[1] + 1] = name
 	end
 	-- 2 - masculine
-	data = assert(io.open('data/pt_BR/firstnames-masculine.csv', 'rb')):read('*all')
+	data = assert(io.open('data/en_US/firstnames-masculine.csv', 'rb')):read('*all')
 	for name in string.gmatch(data, '[%S]+') do
 		firstnames[2][#firstnames[2] + 1] = name
 	end
 	return firstnames
 end
 
-function pt_BR.load_lastnames()
+function en_US.load_lastnames()
 	local lastnames = {} -- avoid some rehashes
-	local data = assert(io.open('data/pt_BR/lastnames.csv', 'rb')):read('*all')
+	local data = assert(io.open('data/en_US/lastnames.csv', 'rb')):read('*all')
 	for name in string.gmatch(data, '[^\n]+') do
 		lastnames[#lastnames + 1] = name
 	end
 	return lastnames
 end
 
-function pt_BR.load_countries()
+function en_US.load_countries()
 	local countries = {}
-	local data = assert(io.open('data/pt_BR/countries.csv', 'rb')):read('*all')
+	local data = assert(io.open('data/en_US/countries.csv', 'rb')):read('*all')
 	for item in string.gmatch(data, '[^\n]+') do
 		countries[#countries + 1] = item
 	end
 	return countries
 end
 
-function pt_BR.load_states()
+function en_US.load_states()
 	local states = {}
-	local data = assert(io.open('data/pt_BR/states.csv', 'rb')):read('*all')
+	local data = assert(io.open('data/en_US/states.csv', 'rb')):read('*all')
 	for item in string.gmatch(data, '[^\n]+') do
 		states[#states + 1] = item
 	end
 	return states
 end
 
-function pt_BR.load_cities()
+function en_US.load_cities()
 	local cities = {}
-	local data = assert(io.open('data/pt_BR/cities.csv', 'rb')):read('*all')
+	local data = assert(io.open('data/en_US/cities.csv', 'rb')):read('*all')
 	for item in string.gmatch(data, '[^\n]+') do
 		cities[#cities + 1] = item
 	end
 	return cities
 end
 
-local cep = {0,0,0,0,0,0,0,0}
-function pt_BR.cep()
-	for i = 1,8 do
-		cep[i] = math.random(1,9)
-	end
-	return string.format('%s%s%s%s%s-%s%s%s', cep[1], cep[2], cep[3], cep[4], cep[5], cep[6], cep[7], cep[8], cep[9])
-end
-
-function pt_BR.cpf()
-	local n = {0,0,0,0,0,0,0,0,0}
-	for i = 1, 9 do
-		n[i] = math.random(1, 9)
-	end
+function en_US.ssn()
 	
-	local d1 = 0
-	for i = 1, 9 do
-		d1 = d1 + n[#n + 1 - i] * (i + 1) -- reverse index
-	end
-	d1 = 11 - (d1 % 11)
-	if d1 >= 10 then d1 = 0	end
-	
-	local d2 = 0
-	n[#n + 1] = d1
-	for i = 1, 10 do
-		d2 = d2 + n[#n + 1 - i] * (i + 1) -- reverse index
-	end
-	d2 = 11 - (d2 % 11)
-	if d2 >= 10 then d2 = 0	end
-	
-	return string.format('%s%s%s.%s%s%s.%s%s%s-%s%s', n[1], n[2], n[3], n[4], n[5], n[6], n[7], n[8], n[9], d1, d2)
 end
 
 local accents = {
@@ -101,8 +73,8 @@ local accents = {
 	['ý'] = 'y', ['ÿ'] = 'y',
 	['Ý'] = 'Y', ['Ÿ'] = 'Y'
 }
-function pt_BR.normalize(str)
+function en_US.normalize(str)
 	return string.gsub(str, '[%z\1-\127\194-\244][\128-\191]*', accents)
 end
 
-return pt_BR
+return en_US
--- test/faker.lua
+++ test/faker.lua
@@ -37,17 +37,73 @@ function Faker.randint(size)
 	end
 end
 
-local faker = Faker:new({locale = 'pt_BR'})
+function Faker:firstname(properties)
+	self.firstnames = self.load_firstnames()
+	function self:firstname(properties)
+		properties = properties or {}
+		local gender = 1
+		if properties.gender == 'masculine' then
+			gender = 2
+		elseif properties.gender ~= 'feminine' then
+			gender = math.random(1, 2)
+		end
+		return self.firstnames[gender][math.random(1, #self.firstnames[gender])]
+	end
+	return self:firstname(properties)
+end
+
+function Faker:lastname()
+	self.lastnames = self.load_lastnames()
+	function self:lastname()
+		return self.lastnames[math.random(1, #self.lastnames)]
+	end
+	return self:lastname()
+end
+
+function Faker:name(properties)
+	return self:firstname(properties or {}) .. ' ' .. self:lastname()
+end
+
+function Faker:email(properties)
+	local username = self:firstname(properties) .. '.' .. string.gsub(self:lastname(), '%s+', '')
+	return string.gsub(string.lower(self.normalize(username)), "'", '') .. '@example.com'
+end
+
+function Faker:country()
+	self.countries = self.load_countries()
+	function self:country()
+		return self.countries[math.random(1, #self.countries)]
+	end
+	return self:country()
+end
+
+function Faker:state()
+	self.states = self.load_states()
+	function self:state()
+		return self.states[math.random(1, #self.states)]
+	end
+	return self:state()
+end
+
+function Faker:city()
+	self.cities = self.load_cities()
+	function self:city()
+		return self.cities[math.random(1, #self.cities)]
+	end
+	return self:city()
+end
+
+local faker = Faker:new({locale = 'en_US'})
 for i = 1, 100 do
 	print(faker.randstring())
 	print(faker.randint(10))
-	print(faker.name())
-	print(faker.email())
-	print(faker.cpf())
-	print(faker.country())
-	print(faker.state())
-	print(faker.city())
-	print(faker.cep())
+	print(faker:name())
+	print(faker:email())
+	-- print(faker.cpf())
+	print(faker:country())
+	print(faker:state())
+	print(faker:city())
+	-- print(faker.cep())
 end
 
 return Faker
